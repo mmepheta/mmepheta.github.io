0bc06443d9df942577925773a736f0b05301cced
Vinicius Nogueira <viniciusln@gmail.com>
2020-05-29 01:54:48 -0300

    auto play do video ao abrir a modal

--- js/mme.js
+++ js/mme.js
@@ -4,15 +4,14 @@ $(function() {
         var $if = $(e.delegateTarget).find('iframe');
         var src = $if.attr("src");
         $if.attr("src", '/empty.html');
-        $if.attr("src", src);
+        $if.attr("src", src.replace("&autoplay=1",""));
     });
 
-
-    $('.videoModal').on('shown.bs.modal', function () {
-      $('#video1')[0].play();
-    })
-    $('.videoModal').on('hidden.bs.modal', function () {
-      $('#video1')[0].pause();
-    })
+    $('.videoModal').on('show.bs.modal', function(e) {
+        var $if = $(e.delegateTarget).find('iframe');
+        var src = $if.attr("src");
+        $if.attr("src", '/empty.html');
+        $if.attr("src", src + '&autoplay=1');
+    });
 
 });
